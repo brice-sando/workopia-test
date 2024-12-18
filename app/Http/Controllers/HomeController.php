3d837252b11e8b2571af9df00d2a159e31eac50a
Brice Sando <brice.sando@gcs-technologie.com>
2024-12-18 03:33:08 -0500

    Index and welcome pages reformat

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class HomeController extends Controller
 {
     function index() {
-        return view("pages.index");
+        $jobs = Job::all();
+        return view("pages.index")->with("jobs", $jobs);
     }
 }
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,11 +1,8 @@
 <x-layout>
     <x-slot name='title'>Available Job Listings</x-slot>
-    <h1 class="text-2xl">Job List</h1>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse ($jobs as $job)
-            <div>
-                {{ $job->title }}
-            </div>
+           <x-job-card :job="$job"/>
         @empty
             <p>No data to display</p>
         @endforelse ($jobs as $job)
